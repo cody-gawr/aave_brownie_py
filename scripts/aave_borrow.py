d96d84c87b62e9076b6626343878c6e8545bd7c1
yhy940806 <yhy940806@163.com>
2021-10-11 21:50:00 -0400

    – get borrowable data

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -1,6 +1,9 @@
 from brownie import network, config, interface
 from scripts.helpful_scripts import FORKED_LOCAL_ENVIRONMENTS, get_account
 from scripts.get_weth import get_weth
+from web3 import Web3
+
+amount = Web3.toWei(0.1, "ether")
 
 
 def main():
@@ -11,7 +14,14 @@ def main():
     # ABI
     # Address
     lending_pool = get_lending_pool()
-    print(lending_pool)
+    approve_erc20(amount, lending_pool.address, erc20_address, account)
+    print("Depositing...")
+    tx = lending_pool.deposit(
+        erc20_address, amount, account.address, 0, {"from": account}
+    )
+    tx.wait(1)
+    print("Deposited!")
+    borrowable_eth, total_debt = get_borrowable_data(lending_pool, account)
 
 
 def get_lending_pool():
@@ -23,7 +33,28 @@ def get_lending_pool():
     return lending_pool
 
 
-def approve_erc20():
-    # ABI
-    # Address
-    pass
+def get_borrowable_data(lending_pool, account):
+    (
+        total_collateral_eth,
+        total_debt_eth,
+        available_borrow_eth,
+        current_liquidation_threshold,
+        ltv,
+        health_factor,
+    ) = lending_pool.getUserAccountData(account.address)
+    available_borrow_eth = Web3.fromWei(available_borrow_eth, "ether")
+    total_collateral_eth = Web3.fromWei(total_collateral_eth, "ether")
+    total_debt_eth = Web3.fromWei(total_debt_eth, "ether")
+    print(f"You have {total_collateral_eth} worth of ETH deposited.")
+    print(f"You have {total_debt_eth} worth of ETH borrowed.")
+    print(f"You can borrow {available_borrow_eth} worth of ETH.")
+    return (float(available_borrow_eth), float(total_debt_eth))
+
+
+def approve_erc20(amount, spender, erc20_address, account):
+    print("Approving ERC20 token...")
+    erc20 = interface.IERC20(erc20_address)
+    tx = erc20.approve(spender, amount, {"from": account})
+    tx.wait(1)
+    print("Approved!")
+    return tx
